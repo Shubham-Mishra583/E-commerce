1ea3ab3b797d4e04450cd2b5658a6a110287b9ed
Shubham Mishra <shubhamishra583@gmail.com>
2022-12-17 15:18:00 +0530

    c

---
+++ src/styles/Button.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  text-decoration: none;
+  max-width: auto;
+  /* background-color: rgb(98 84 243); */
+  background-color: rgb(255 77 77);
+  /* background-color: #5D92F4; */
+  color: rgb(255 255 255);
+  padding: 1.4rem 2.4rem;
+  border: none;
+  border-radius: 10px;
+  text-transform: uppercase;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  -webkit-transition: all 0.3s ease 0s;
+  -moz-transition: all 0.3s ease 0s;
+  -o-transition: all 0.3s ease 0s;
+  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+
+  &:hover,
+  &:active {
+    box-shadow: 0 2rem 2rem 0 rgb(132 144 255 / 30%);
+    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+    transform: scale(0.96);
+  }
+
+  a {
+    text-decoration: none;
+    color: rgb(255 255 255);
+    font-size: 1.8rem;
+  }
+`;
